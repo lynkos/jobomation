e467ba022e16b3e7c269d899fd6161d8bad15a19
Kiran Brahmatewari <kiwi2mii@gmail.com>
2026-08-11 19:21:03 -0400

    Refactor Greenhouse collector

--- src/jobomation/collectors/greenhouse.py
+++ src/jobomation/collectors/greenhouse.py
@@ -11,6 +11,19 @@ def clean_description(content: str) -> str:
     soup = BeautifulSoup(decoded, "html.parser")
     return soup.get_text(separator="\n", strip=True)
 
+def _raw_to_job(raw: dict) -> Job:
+    return Job(
+        source=JOB_COLLECTOR_NAME,
+        source_job_id=str(raw["id"]),
+        title=raw["title"],
+        company=raw["company_name"],
+        location=raw["location"]["name"],
+        url=raw["absolute_url"],
+        first_published=raw["first_published"],
+        updated_at=raw["updated_at"],
+        description=clean_description(raw["content"])
+    )
+
 # Fetch job by ID
 def fetch_job(board: str, job_id: int | str) -> Job:
     url = f"{JOB_COLLECTOR_URL}/{board}/jobs/{job_id}"
@@ -19,17 +32,7 @@ def fetch_job(board: str, job_id: int | str) -> Job:
     response.raise_for_status()
     raw_job = response.json()
 
-    return Job(
-            source=JOB_COLLECTOR_NAME,
-            source_job_id=str(raw_job["id"]),
-            title=raw_job["title"],
-            company=raw_job["company_name"],
-            location=raw_job["location"]["name"],
-            url=raw_job["absolute_url"],
-            first_published=raw_job["first_published"],
-            updated_at=raw_job["updated_at"],
-            description=clean_description(raw_job["content"])
-        )
+    return _raw_to_job(raw_job)
 
 # All currently published jobs
 def fetch_jobs(board: str) -> list[Job]:
@@ -43,17 +46,4 @@ def fetch_jobs(board: str) -> list[Job]:
 
     raw_jobs = response.json()["jobs"]
 
-    return [
-        Job(
-            source=JOB_COLLECTOR_NAME,
-            source_job_id=str(raw["id"]),
-            title=raw["title"],
-            company=raw["company_name"],
-            location=raw["location"]["name"],
-            url=raw["absolute_url"],
-            first_published=raw["first_published"],
-            updated_at=raw["updated_at"],
-            description=clean_description(raw["content"])
-        )
-        for raw in raw_jobs
-    ]
+    return [_raw_to_job(raw) for raw in raw_jobs]
