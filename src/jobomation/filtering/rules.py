4245c92241fe6fd7954d4f7cde52859f066fb85d
Kiran Brahmatewari <kiwi2mii@gmail.com>
2026-08-11 20:33:13 -0400

    Update whitespace

--- src/jobomation/filtering/rules.py
+++ src/jobomation/filtering/rules.py
@@ -1,11 +1,7 @@
 from re import search, IGNORECASE
 from jobomation.models import Job
 
-
-def apply_title_filter(
-    job: Job,
-    title_filters: dict[str, list[str]],
-) -> Job:
+def apply_title_filter(job: Job, title_filters: dict[str, list[str]]) -> Job:
     title = job.title.casefold()
 
     for reason, patterns in title_filters.items():
@@ -31,11 +27,5 @@ def apply_filters(job: Job, filters: dict) -> Job:
 
     return job
 
-def apply_filters_to_jobs(
-    jobs: list[Job],
-    filters: dict,
-) -> list[Job]:
-    return [
-        apply_filters(job, filters)
-        for job in jobs
-    ]
+def apply_filters_to_jobs(jobs: list[Job], filters: dict) -> list[Job]:
+    return [ apply_filters(job, filters) for job in jobs ]
